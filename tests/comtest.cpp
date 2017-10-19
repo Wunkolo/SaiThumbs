bc79e383618c99a17c206fc97a9560be6a8c4ef8
Wunkolo <Wunkolo@gmail.com>
2017-10-19 14:15:27 -0700

    Update COM testbed verbosity

--- tests/comtest.cpp
+++ tests/comtest.cpp
@@ -1,3 +1,7 @@
+#include <cstdint>
+#include <cstddef>
+#include <cstdio>
+
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 
@@ -6,20 +10,93 @@
 #include <Thumbcache.h>
 #include <Unknwn.h>
 
-#include <stdio.h>
-
 #pragma comment(lib, "Shlwapi.lib")
 #pragma comment(lib, "Ole32.lib")
 
-LPCOLESTR myGuid = L"{35788B9B-04AB-4B75-AB3A-1ED403AFC746}";
+const wchar_t* ProviderGUID = L"{35788B9B-04AB-4B75-AB3A-1ED403AFC746}";
 
-typedef HRESULT ourDllGetClassObjectT(REFCLSID rclsid, REFIID riid, void** ppv);
+using DllGetClassObjectT = HRESULT(const IID& rclsid, const IID& riid, void** ppv);
+
+BOOL SaveHBITMAPToFile(HBITMAP Bitmap, const char* Filename);
+
+int main(int argc, char** argv)
+{
+	GUID clsid;
+	IIDFromString(ProviderGUID, &clsid);
+
+	HMODULE DLLHandle = LoadLibraryA("SaiThumbs.dll");
+
+	if( !DLLHandle )
+	{
+		std::puts("can't open DLL");
+		return EXIT_FAILURE;
+	}
+
+	DllGetClassObjectT* DllGetClassObject = reinterpret_cast<DllGetClassObjectT*>(GetProcAddress(
+		DLLHandle, "DllGetClassObject"));
+
+	if( DllGetClassObject == nullptr )
+	{
+		std::puts("Failed: Unable to get DLLGetClassobject address");
+		return EXIT_FAILURE;
+	}
+
+	IClassFactory* ClassFactory = nullptr;
+	HRESULT Result = DllGetClassObject(clsid, IID_IClassFactory, reinterpret_cast<void**>(&ClassFactory));
+	if( Result != S_OK )
+	{
+		std::printf("Failed: Unable to get IClassFactory: %08x\n", Result);
+		return EXIT_FAILURE;
+	}
+
+	IInitializeWithFile* InitWithFile;
+	Result = ClassFactory->CreateInstance(nullptr, IID_IInitializeWithFile, reinterpret_cast<void**>(&InitWithFile));
+	if( Result != S_OK )
+	{
+		std::puts("Failed: Unable to get IInitializeWithFile");
+		return EXIT_FAILURE;
+	}
+	ClassFactory->Release();
+
+	IThumbnailProvider* ThumbProvider;
+	Result = InitWithFile->QueryInterface(IID_IThumbnailProvider, reinterpret_cast<void**>(&ThumbProvider));
+	if( Result != S_OK )
+	{
+		std::puts("Failed: Unable to get IThumbnailProvider");
+		return EXIT_FAILURE;
+	}
+
+	Result = InitWithFile->Initialize(L"./test.sai", 0);
+	InitWithFile->Release();
+	if( Result != S_OK )
+	{
+		std::puts("Failed: Unable to initilize IInitializeWithFile");
+		return EXIT_FAILURE;
+	}
+
+	HBITMAP ThumbImage;
+	WTS_ALPHATYPE ThumbAlphaType;
+	Result = ThumbProvider->GetThumbnail(256, &ThumbImage, &ThumbAlphaType);
+	ThumbProvider->Release();
+	if( Result != S_OK )
+	{
+		std::puts("Failed: GetThumbnail returned failure");
+		return EXIT_FAILURE;
+	}
+
+	SaveHBITMAPToFile(
+		ThumbImage,
+		"./test.bmp"
+	);
+
+	return EXIT_SUCCESS;
+}
 
 BOOL SaveHBITMAPToFile(HBITMAP Bitmap, const char* Filename)
 {
 	const DWORD PaletteSize = 0;
-	DWORD BmBitsSize = 0, DibSize = 0, Written = 0;
-	BITMAP Bitmap0;
+	DWORD BmBitsSize = 0, HeaderSize = 0, Written = 0;
+	BITMAP NewBitmap;
 	BITMAPFILEHEADER Header;
 	BITMAPINFOHEADER bi;
 	HANDLE hOldPal2 = nullptr;
@@ -27,10 +104,10 @@ BOOL SaveHBITMAPToFile(HBITMAP Bitmap, const char* Filename)
 	const int Bits = GetDeviceCaps(DeviceContext, BITSPIXEL) * GetDeviceCaps(DeviceContext, PLANES);
 	DeleteDC(DeviceContext);
 	const WORD BitCount = Bits;
-	GetObject(Bitmap, sizeof(Bitmap0), reinterpret_cast<LPSTR>(&Bitmap0));
+	GetObject(Bitmap, sizeof(NewBitmap), reinterpret_cast<char*>(&NewBitmap));
 	bi.biSize = sizeof(BITMAPINFOHEADER);
-	bi.biWidth = Bitmap0.bmWidth;
-	bi.biHeight = -Bitmap0.bmHeight;
+	bi.biWidth = NewBitmap.bmWidth;
+	bi.biHeight = -NewBitmap.bmHeight;
 	bi.biPlanes = 1;
 	bi.biBitCount = BitCount;
 	bi.biCompression = BI_RGB;
@@ -39,8 +116,8 @@ BOOL SaveHBITMAPToFile(HBITMAP Bitmap, const char* Filename)
 	bi.biYPelsPerMeter = 0;
 	bi.biClrImportant = 0;
 	bi.biClrUsed = 256;
-	BmBitsSize = ((Bitmap0.bmWidth * BitCount + 31) & ~31) / 8
-		* Bitmap0.bmHeight;
+	BmBitsSize = ((NewBitmap.bmWidth * BitCount + 31) & ~31) / 8
+		* NewBitmap.bmHeight;
 	const HANDLE hDib = GlobalAlloc(GHND, BmBitsSize + PaletteSize + sizeof(BITMAPINFOHEADER));
 	const LPBITMAPINFOHEADER lpbi = static_cast<LPBITMAPINFOHEADER>(GlobalLock(hDib));
 	*lpbi = bi;
@@ -54,8 +131,8 @@ BOOL SaveHBITMAPToFile(HBITMAP Bitmap, const char* Filename)
 	}
 
 	GetDIBits(
-		DeviceContext, Bitmap, 0, static_cast<UINT>(Bitmap0.bmHeight),
-		reinterpret_cast<LPSTR>(lpbi) + sizeof(BITMAPINFOHEADER)
+		DeviceContext, Bitmap, 0, static_cast<std::uint32_t>(NewBitmap.bmHeight),
+		reinterpret_cast<char*>(lpbi) + sizeof(BITMAPINFOHEADER)
 		+ PaletteSize, reinterpret_cast<BITMAPINFO *>(lpbi), DIB_RGB_COLORS);
 
 	if( hOldPal2 )
@@ -65,99 +142,28 @@ BOOL SaveHBITMAPToFile(HBITMAP Bitmap, const char* Filename)
 		ReleaseDC(nullptr, DeviceContext);
 	}
 
-	HANDLE fh = CreateFile(
+	const HANDLE FileHandle = CreateFileA(
 		Filename, GENERIC_WRITE, 0, nullptr, CREATE_ALWAYS,
 		FILE_ATTRIBUTE_NORMAL | FILE_FLAG_SEQUENTIAL_SCAN, nullptr);
 
-	if( fh == INVALID_HANDLE_VALUE )
+	if( FileHandle == INVALID_HANDLE_VALUE )
 	{
 		return FALSE;
 	}
 
-	Header.bfType = 0x4D42; // "BM"
-	DibSize = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER) + PaletteSize + BmBitsSize;
-	Header.bfSize = DibSize;
+	Header.bfType = 0x4D42;
+	HeaderSize = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER) + PaletteSize + BmBitsSize;
+	Header.bfSize = HeaderSize;
 	Header.bfReserved1 = 0;
 	Header.bfReserved2 = 0;
-	Header.bfOffBits = static_cast<DWORD>(sizeof(BITMAPFILEHEADER)) + static_cast<DWORD>(sizeof(BITMAPINFOHEADER)) +
+	Header.bfOffBits = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER) +
 		PaletteSize;
 
-	WriteFile(fh, reinterpret_cast<LPSTR>(&Header), sizeof(BITMAPFILEHEADER), &Written, nullptr);
+	WriteFile(FileHandle, reinterpret_cast<char*>(&Header), sizeof(BITMAPFILEHEADER), &Written, nullptr);
 
-	WriteFile(fh, reinterpret_cast<LPSTR>(lpbi), DibSize, &Written, nullptr);
+	WriteFile(FileHandle, reinterpret_cast<char*>(lpbi), HeaderSize, &Written, nullptr);
 	GlobalUnlock(hDib);
 	GlobalFree(hDib);
-	CloseHandle(fh);
+	CloseHandle(FileHandle);
 	return TRUE;
-}
-
-int main(int argc, char** argv)
-{
-	GUID clsid = {
-		0
-	};
-	IIDFromString(myGuid, &clsid);
-
-	HRESULT r;
-	HMODULE dll = nullptr;
-
-	dll = LoadLibraryA("SaiThumbs.dll");
-	if( !dll )
-	{
-		printf("can't open DLL\n");
-		return 1;
-	}
-
-	ourDllGetClassObjectT* ourDllGetClassObject = reinterpret_cast<ourDllGetClassObjectT*>(GetProcAddress(
-		dll, "DllGetClassObject"));
-
-	IClassFactory* pFactory = nullptr;
-	r = ourDllGetClassObject(clsid, IID_IClassFactory, reinterpret_cast<void**>(&pFactory));
-	if( r != S_OK )
-	{
-		printf("failed: get factory: %08x\n", r);
-		return 2;
-	}
-
-	IInitializeWithFile* InitWithFile;
-	r = pFactory->CreateInstance(nullptr, IID_IInitializeWithFile, reinterpret_cast<void**>(&InitWithFile));
-	if( r != S_OK )
-	{
-		printf("failed: get object\n");
-		return 3;
-	}
-	pFactory->Release();
-
-	IThumbnailProvider* pProvider;
-	r = InitWithFile->QueryInterface(IID_IThumbnailProvider, reinterpret_cast<void**>(&pProvider));
-	if( r != S_OK )
-	{
-		printf("failed: get provider\n");
-		return 5;
-	}
-
-	r = InitWithFile->Initialize(L"./test.sai", 0);
-	InitWithFile->Release();
-	if( r != S_OK )
-	{
-		printf("failed: init provider\n");
-		return 11;
-	}
-
-	HBITMAP bmp;
-	WTS_ALPHATYPE alpha;
-	r = pProvider->GetThumbnail(256, &bmp, &alpha);
-	pProvider->Release();
-	if( r != S_OK )
-	{
-		printf("failed: make thumbnail\n");
-		return 12;
-	}
-
-	SaveHBITMAPToFile(
-		bmp,
-		"./test.bmp"
-	);
-
-	printf("done");
 }
