5fa87378b974dc70cb000a1142e630f58bdd598a
Wunkolo <Wunkolo@gmail.com>
2025-11-15 18:35:19 -0800

    Refactor registry handling for multiple thumbnail handlers

--- include/Config.hpp
+++ include/Config.hpp
@@ -6,20 +6,14 @@
 namespace SaiThumb
 {
 
-#define SaiThumbHandlerCLSID L"{35788B9B-04AB-4B75-AB3A-1ED403AFC746}"
-#define SaiThumbHandlerName L"Sai Thumb Handler"
-#define SaiThumbHandlerExtension L".sai"
+#define Sai1ThumbHandlerCLSID L"{35788B9B-04AB-4B75-AB3A-1ED403AFC746}"
+#define Sai1ThumbHandlerName L"Sai1 Thumb Handler"
+#define Sai1ThumbHandlerExtension L".sai"
+
+#define Sai2ThumbHandlerCLSID L"{A7E68441-D892-40FA-B782-5144380A0875}"
+#define Sai2ThumbHandlerName L"Sai2 Thumb Handler"
+#define Sai2ThumbHandlerExtension L".sai2"
 
 #define IThumbnailProviderCLSID L"{E357FCCD-A995-4576-B01F-234630154E96}"
-
-/// Registry
-struct RegistryEntry
-{
-	HKEY           Root;
-	const wchar_t* KeyName;
-	const wchar_t* KeyValue;
-	DWORD          ValueType;
-	const wchar_t* Data;
-};
 
 } // namespace SaiThumb
